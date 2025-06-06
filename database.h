02069c7b1b2437c4f82f8686c6b53335acfe6895
colte <rdaanyaal@gmail.com>
2025-06-06 11:50:38 +0200

    changed main.c

--- database.h
+++ database.h
@@ -4,7 +4,75 @@
 #define MAX_PAIRS 100
 #define MAX_KEY_LEN 50
 #define MAX_VALUE_LEN 100
+typedef struct {
+    char key[MAX_KEY_LEN];
+    char value[MAX_VALUE_LEN];
+} KeyValuePair;
 
+KeyValuePair store[MAX_PAIRS];
+int storeSize = 0;
+
+void trim_newline(char *str) {
+    str[strcspn(str, "\r\n")] = 0;
+}
+
+int put(const char *key, const char *value) {
+    // check if
+    for (int i = 0; i < storeSize; ++i) {
+        if (strcmp(store[i].key, key) == 0) {
+            strncpy(store[i].value, value, MAX_VALUE_LEN);
+            return 2;
+        }
+    }
+
+    // Add new key and val
+    if (storeSize < MAX_PAIRS) {
+        strncpy(store[storeSize].key, key, MAX_KEY_LEN);
+        strncpy(store[storeSize].value, value, MAX_VALUE_LEN);
+        storeSize++;
+    } else {
+        printf("Store full!\n");
+    }
+}
+
+// Get value by key
+int get(const char *key) {
+    char tempString[1024];
+    printf("GET Function accessed \n");
+    for (int i = 0; i < strlen(key) - 1; ++i) {
+        printf("%c\n", key[i]);
+        tempString[i] = key[i];
+    }
+    for (int i = 0; i < storeSize; i++) {
+        if (strcmp(store[i].key, tempString) == 0) {
+            printf("Found!\n");
+            printf("Value : %s\n", store[i].value);
+            printf("Key : %s\n", store[i].key);
+            printf("StoreIndex : %d\n", i);
+            return 1;
+        }
+    }
+    printf("Not Found-\n");
+    return 0;
+}
+// delete bny key
+int del(const char *key) {
+    char trimmedKey[MAX_KEY_LEN];
+    strncpy(trimmedKey, key, MAX_KEY_LEN - 1);
+    trimmedKey[MAX_KEY_LEN - 1] = '\0';
+    trim_newline(trimmedKey);
+
+    for (int i = 0; i < storeSize; i++) {
+        if (strcmp(store[i].key, trimmedKey) == 0) {
+            for (int j = i; j < storeSize - 1; j++) {
+                store[j] = store[j + 1];
+            }
+            storeSize--;
+            return 1;  // Deleted
+        }
+    }
+    return 0;  // Not found
+}
 // typedef struct {
 //     char key[MAX_KEY_LEN];
 //     char value[MAX_VALUE_LEN];
--- main.c
+++ main.c
@@ -1,153 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include "database.h"
-
-
-typedef struct {
-    char key[MAX_KEY_LEN];
-    char value[MAX_VALUE_LEN];
-} KeyValuePair;
-
-KeyValuePair store[MAX_PAIRS];
-int storeSize = 0;
-
-void trim_newline(char *str) {
-    str[strcspn(str, "\r\n")] = 0;
-}
-
-
-// Set key-value
-int put(const char *key, const char *value) {
-    // Check if key already exists; if so, update
-    for (int i = 0; i < storeSize; ++i) {
-        if (strcmp(store[i].key, key) == 0) {
-            strncpy(store[i].value, value, MAX_VALUE_LEN);
-            return 2;
-        }
-    }
-
-    // Add new key-value pair
-    if (storeSize < MAX_PAIRS) {
-        strncpy(store[storeSize].key, key, MAX_KEY_LEN);
-        strncpy(store[storeSize].value, value, MAX_VALUE_LEN);
-        storeSize++;
-    } else {
-        printf("Store full!\n");
-    }
-}
-
-// Get value by key
-int get(const char *key) {
-    char tempString[1024];
-    printf("GET Function accessed \n");
-    for (int i = 0; i < strlen(key) - 1; ++i) {
-        printf("%c\n", key[i]);
-        tempString[i] = key[i];
-    }
-    for (int i = 0; i < storeSize; i++) {
-        if (strcmp(store[i].key, tempString) == 0) {
-            printf("Found!\n");
-            printf("Value : %s\n", store[i].value);
-            printf("Key : %s\n", store[i].key);
-            printf("StoreIndex : %d\n", i);
-            return 1;
-        }
-    }
-    printf("Not Found-\n");
-    return 0;
-}
-
-int del(const char *key) {
-    char trimmedKey[MAX_KEY_LEN];
-    strncpy(trimmedKey, key, MAX_KEY_LEN - 1);
-    trimmedKey[MAX_KEY_LEN - 1] = '\0';
-    trim_newline(trimmedKey);
-
-    for (int i = 0; i < storeSize; i++) {
-        if (strcmp(store[i].key, trimmedKey) == 0) {
-            for (int j = i; j < storeSize - 1; j++) {
-                store[j] = store[j + 1];
-            }
-            storeSize--;
-            return 1;  // Deleted
-        }
-    }
-    return 0;  // Not found
-}
+#define PORT 99889
+#define MAX_CLIENTS  FD_SETSIZE
+#define BUFFER_SIZE  1024
 
 int main() {
-    // Server Socket
-    fd_set CurrentSockets, ReadySockets;
-    FD_ZERO(&CurrentSockets);
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        printf("ERROR opening socket\n");
+    int listen_fd, new_fd, max_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len;
+
+    fd_set master_set, read_set;
+
+
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
-    FD_SET(sockfd,  &CurrentSockets);
 
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(1234);
 
-    // char buffer[1024];
-    int addressLength = sizeof(address);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    bind(sockfd, (struct sockaddr*)&address, addressLength) ? printf("ERROR on bind\n") : printf("OK on bind\n");
 
-    listen(sockfd, 5) ? printf("ERROR on listen\n") : printf("OK on listen\n");
+    if (bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        close(listen_fd);
+        exit(EXIT_FAILURE);
+    }
 
-    // int client_socket = accept(sockfd, (struct sockaddr*)&address, (socklen_t*)&addressLength);
-    // if (client_socket < 0) {
-    //     printf("ERROR on accept\n");
-    // }
-    // write(client_socket, "Hello World!\n", 14);
-    // char string[1024] = {0};  // Buffer to store input
-    // int i = 0;                // Current position in the buffer
+
+    if (listen(listen_fd, 10) < 0) {
+        perror("listen failed");
+        close(listen_fd);
+        exit(EXIT_FAILURE);
+    }
+
+
+    FD_ZERO(&master_set);
+    FD_SET(listen_fd, &master_set);
+    max_fd = listen_fd;
+
+    printf("Server listening on port %d...\n", PORT);
+
 
     while (1) {
-        ReadySockets = CurrentSockets;
-        if(select(FD_SETSIZE, &ReadySockets, NULL, NULL, NULL) < 0) {
-            printf("ERROR on select\n");
-            perror("select");
-            exit(1);
+        read_set = master_set;
+
+        if (select(max_fd + 1, &read_set, NULL, NULL, NULL) < 0) {
+            perror("select failed");
+            exit(EXIT_FAILURE);
         }
-        printf("select done\n");
 
-        for(int clientIndex = 0; clientIndex < FD_SETSIZE; clientIndex++) {
-            if(FD_ISSET(clientIndex, &ReadySockets)) {
-                if(clientIndex == sockfd) {
 
-                    int client_socket = accept(sockfd, (struct sockaddr*)&address, (socklen_t*)&addressLength);
-                    if (client_socket < 0) {
-                        printf("ERROR on accept\n");
+        for (int fd = 0; fd <= max_fd; fd++) {
+            if (FD_ISSET(fd, &read_set)) {
+                if (fd == listen_fd) {
+
+                    addr_len = sizeof(client_addr);
+                    new_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &addr_len);
+                    if (new_fd < 0) {
+                        perror("accept failed");
+                        continue;
                     }
-                    printf("%d accept done\n", clientIndex);
-                    FD_SET(client_socket, &CurrentSockets);
-                } else {
+                    FD_SET(new_fd, &master_set);
+                    if (new_fd > max_fd) max_fd = new_fd;
 
-                    printf("%d client\n", clientIndex);
-                    char string[1024] = {0};  // Buffer to store input
+                    printf("New connection from %s:%d (fd: %d)\n",
+                        inet_ntoa(client_addr.sin_addr),
+                        ntohs(client_addr.sin_port),
+                        new_fd);
+
+                } else {
+                    char string[1024] = {0};
                     int j = 0;
                     char buffer[1024];
                     while(1) {
-                        int bytesRead = read(clientIndex, buffer, 1024);
+                        int bytesRead = read(fd, buffer, 1024);
         if (bytesRead < 0) {
             printf("ERROR on read\n");
-            close(clientIndex);
-            FD_CLR(clientIndex, &CurrentSockets);
+            close(fd);
+            FD_CLR(fd, &master_set);
             break;
         }
-        buffer[bytesRead] = '\0';  // Null-terminate the received data
+        buffer[bytesRead] = '\0';
 
-        // Process each received byte
+
         for (int u = 0; u < bytesRead; u++) {
-            if (buffer[u] == '\n') {  // Enter key detected
-                string[j] = '\0';    // Terminate the string
+            if (buffer[u] == '\n') {
+                string[j] = '\0';
                 printf("Full input: %s\n", string);
 
                 // Check if input is "quit"
@@ -156,10 +106,10 @@ int main() {
               string[1] == 'u' &&
               string[2] == 'i' &&
               string[3] == 't') {
-                    string[j] = '\0';  // Properly terminate the string
+                    string[j] = '\0';
                     printf("Received quit command\n");
-                    FD_CLR(clientIndex, &CurrentSockets);
-                    break;  // or use another method to break out
+                    FD_CLR(fd, &master_set);
+                    break;
               }
 
                 // Check if input starts with "put"
@@ -220,7 +170,7 @@ int main() {
                 j = 0;  // Reset buffer for next input
             } else {
                 string[j++] = buffer[u];  // Store the character
-                write(clientIndex, buffer, strlen(buffer) - 1);
+                write(fd, buffer, strlen(buffer) - 1);
                 // Prevent buffer overflow
                 if (j >= sizeof(string) - 1) {
                     j = 0;  // Reset or handle overflow
@@ -231,17 +181,10 @@ int main() {
 
         memset(buffer, 0, 1024);  // Clear the read buffer
                     }
-                    FD_CLR(clientIndex, &CurrentSockets);
                 }
             }
         }
-
-
-    }
-
-    exit_loop:
-
-    close(sockfd);
-
+        }
+    close(listen_fd);
     return 0;
 }
